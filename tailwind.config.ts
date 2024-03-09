2a7956718951f1c9d20feee5f8864b475c43e15e
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2024-03-09 12:56:56 -0300

    prittier and source code pro font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -6,5 +6,5 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-};
-export default config;
+}
+export default config
