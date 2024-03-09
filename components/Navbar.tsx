00df69aca3cb0f48ac882ce70c2c4326652de2d6
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2024-03-09 13:09:54 -0300

    basic navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,53 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <span>Bruno Mistro</span>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="h-5 w-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div id="navbar-default">
+          <ul>
+            <li>
+              <a href="#" aria-current="page">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
